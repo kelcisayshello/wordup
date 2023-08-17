30c4a9739cc64539726f5cb0fe41faee922d6193
Kelci Mensah <kelcimensah@gmail.com>
2023-08-17 18:17:23 -0400

    💡 added working theme toggle

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Toggle from './components/Toggle'
+import './App.scss'
+
+export default function App() {
+  return (
+    <>
+      <h1 className="title-app">Word<span>Up</span></h1>
+      <Toggle />
+    </>
+  )
+}
